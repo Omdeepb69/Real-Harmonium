f1b10d69063eae2aaed7a497fd181c8751cd530c
Omdeepb69 <omdeeborkar@gmail.com>
2026-03-12 15:48:08 +0530

    Add one-line installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo ""
+echo "  Installing Real Harmonium..."
+echo ""
+sudo add-apt-repository ppa:omdeepb69/realhm -y
+sudo apt update
+sudo apt install realharmonium -y
+echo ""
+echo "  Done! Run:  realhm"
+echo ""
